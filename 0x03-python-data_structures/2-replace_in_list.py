47205034acd442cf85c2d006f97c3f74a3d493c0
SAAJEVES <eveshofase@gmail.com>
2023-09-10 16:29:33 +0100

    replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    i = 0
+    if idx > len(my_list) or idx < 0:
+        return my_list
+    else:
+        for num in my_list:
+            if i == idx:
+                my_list[idx] = element
+                return my_list
+            i += 1
