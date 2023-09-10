7eca3cdf0268b91b81a6f8ca031714383cc2d3f9
SAAJEVES <eveshofase@gmail.com>
2023-09-11 00:56:40 +0100

    add 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as mod1
+    import add_0 as m
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, mod1.add(a, b)))
+    print("{} + {} = {}".format(a, b, m.add(a, b)))
