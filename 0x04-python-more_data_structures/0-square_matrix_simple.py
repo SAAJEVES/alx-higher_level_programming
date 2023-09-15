896b0e34b5a4b613bd028460eba1b1dfe7aeb573
SAAJEVES <eveshofase@gmail.com>
2023-09-15 23:46:26 +0100

    square matrix2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """This function modifies a two dimension list"""
-    if len(matrix) == 0:
-        reture None
-    else:
-        num = []
-        for row in matrix:
-            a = list(map(lambda x: x * x, row))
-            num.append(a)
-        return num
+    num = []
+    for row in matrix:
+        a = list(map(lambda x: x * x, row))
+        num.append(a)
+    return num
