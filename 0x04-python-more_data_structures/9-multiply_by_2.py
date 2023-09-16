e736337ad5413b7b699e24a8fe94ca66d6d766d4
SAAJEVES <eveshofase@gmail.com>
2023-09-16 23:55:15 +0100

    multiply by 21

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     """multiply values by 2"""
-    for val in a_dictionary.keys():
-        a_dictionary[val] = a_dictionary[val] * 2
+    for key, val in a_dictionary.items():
+        a_dictionary[key] = val * 2
     return a_dictionary
