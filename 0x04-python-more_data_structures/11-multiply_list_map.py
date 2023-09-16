9dc11d70670099172508e28a04aa7e96fdfcbc68
SAAJEVES <eveshofase@gmail.com>
2023-09-17 00:32:48 +0100

    10-11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """multiply each elements in a list by a particular value"""
     return list(map(lambda x: x * number, my_list))
