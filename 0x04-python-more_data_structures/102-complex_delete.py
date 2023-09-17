ccca250611538546603475c1ca0828a4ad26a8c6
SAAJEVES <eveshofase@gmail.com>
2023-09-17 01:08:07 +0100

    complex delete1

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     """delete keys with specific value"""
-    for key, val in a_dictionary.items():
+    new_dict = a_dictionary.copy
+    for key, val in new_dict.items():
         if val == value:
             del a_dictionary[key]
     return a_dictionary
