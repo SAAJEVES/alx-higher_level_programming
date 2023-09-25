b13ca1acf530660e50660c0edf4c5420acef73a0
SAAJEVES <eveshofase@gmail.com>
2023-09-25 23:31:54 +0100

    safe print list2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """Print x elements in a list
+
+    Args:
+        my_list (list): the type list to print
+        x (int): the number of integers to print
+
+    Returns:
+        The number of elements printed
+    """
     if my_list == []:
         return None
     num = 0
@@ -12,6 +21,6 @@ def safe_print_list(my_list=[], x=0):
         if num == x:
             break;
     print()
-    return my_list[num - 1]
+    return num - 1
 
 my_list = [1, 2, 3, 4, 5]
