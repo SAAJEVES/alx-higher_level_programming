d53e9d3df4347ab220eaa5fa7b120745d423e5e2
SAAJEVES <eveshofase@gmail.com>
2023-09-25 23:46:08 +0100

    safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Safe print integer
+
+    Args:
+        value (any type): could be of any type
+
+    Returns:
+        An integer if value is an integer
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
