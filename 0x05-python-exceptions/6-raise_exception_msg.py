d1821188a106467c2ebacd1945951c394da8d577
SAAJEVES <eveshofase@gmail.com>
2023-09-26 02:12:41 +0100

    raise exception message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """function that raises a name exception with a message
+
+    Args:
+        message (string): the string message
+
+    Return:
+        raises an exception and print to standard error a customized message
+    """
+    raise Exception(message)
