85f2584d0fee7efb7d9ea1362ff48f82ce544290
SAAJEVES <eveshofase@gmail.com>
2023-09-27 16:46:34 +0100

    empty square class5

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,8 @@
-
 #!/usr/bin/python3
 
-"""This module contains a class called Square"""
+"""Define a class called Square"""
 
 
 class Square:
-    """An empty class Square"""
+    """An empty square class"""
     pass
