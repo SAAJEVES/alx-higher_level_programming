7c0eb2341ecbc2428a464b2246c93f2472eafc7f
SAAJEVES <eveshofase@gmail.com>
2023-10-10 15:56:59 +0100

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''a function that returns the JSON representation of an object (string)'''
+import json
+
+def to_json_string(my_obj):
+    '''returns the JSON representation of an object
+
+    Args:
+        my_obj: python object
+
+    Returns:
+        returns a JSON representation
+    '''
+    json_string = json.dumps(my_obj)
+    return json_string
