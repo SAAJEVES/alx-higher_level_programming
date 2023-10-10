331ecdf76e2eb06d30374dd0b92e01f2dfea2a13
SAAJEVES <eveshofase@gmail.com>
2023-10-10 16:16:01 +0100

    json string to python data1

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -12,5 +12,5 @@ def from_json_string(my_str):
     Returns:
         returns an object (python data structure)
     '''
-    python_str = json.load(my_str)
+    python_str = json.loads(my_str)
     return python_str
