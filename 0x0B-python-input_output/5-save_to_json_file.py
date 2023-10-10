8498155f224949004a690ee20f37bac00d391f49
SAAJEVES <eveshofase@gmail.com>
2023-10-10 18:08:02 +0100

    save to json file3

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
     Returns:
         Nothing
     '''
-    with open(filename, mode='a', encoding='utf-8') as file:
+    with open(filename, mode='w', encoding='utf-8') as file:
         json.dump(my_obj, file)
