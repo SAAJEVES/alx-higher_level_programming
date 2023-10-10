e459d4ef96476f6300f5ad40e0c8ef169503501d
SAAJEVES <eveshofase@gmail.com>
2023-10-10 18:44:00 +0100

    add item1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,15 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+def main():
+    filename = "add_item.json"
 
-if (len(sys.argv) == 1 and os.path.isfile(filename) == False):
-    save_to_json_file([], filename)
-else:
-    python_obj = load_from_json_file(filename)
-    new_python_obj = python_obj + sys.argv[1:]
-    save_to_json_file(new_python_obj, filename)
+    if (len(sys.argv) == 1 and os.path.isfile(filename) == False):
+        save_to_json_file([], filename)
+    else:
+        python_obj = load_from_json_file(filename)
+        new_python_obj = python_obj + sys.argv[1:]
+        save_to_json_file(new_python_obj, filename)
 
+if __name__ == "__main__":
+    main()
