993152bcc4685b91d1496df492e602da7977aa6d
SAAJEVES <eveshofase@gmail.com>
2023-10-13 03:51:45 +0100

    json string to python data2

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-'''function that returns an object (Python data structure) represented by a JSON string'''
+'''function that returns an object
+(Python data structure) represented by a JSON string'''
 import json
+
 
 def from_json_string(my_str):
     '''returns an object represented by a JSON string
-    
+
     Args:
         my_str: json strinfs
 
