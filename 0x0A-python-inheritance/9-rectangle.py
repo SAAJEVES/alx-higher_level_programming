60efb7453528e918203b8a731ffb1aedab4ccacb
SAAJEVES <eveshofase@gmail.com>
2023-10-13 02:13:12 +0100

    9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''Creating a Reactangle class'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class inheriting from BaseGeometry class'''
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
+    
+    def area(self):
+        '''Area of Rectangle'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''Printed Rectangle Class'''
+        return f"[Rectangle] {self.__width}/{self.__height}"
