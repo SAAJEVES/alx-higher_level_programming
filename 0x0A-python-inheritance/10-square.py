814c9154b833eeeb50ea3f58f7d3b11a7e6a3be0
SAAJEVES <eveshofase@gmail.com>
2023-10-13 02:30:31 +0100

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''Creating A Square Class'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class Square that inherits from Rectangle'''
+
+    def __init__(self, size):
+        '''initialization method'''
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''Area of Square'''
+        return self.__size * self.__size
+
+    def __str__(self):
+        '''Printed Object'''
+        return f"[{Recatngle}] {self.__size}/{self.__size}"
