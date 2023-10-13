a17c03fe717197871aa589273750c96e59e08f84
SAAJEVES <eveshofase@gmail.com>
2023-10-13 03:40:44 +0100

    read file4

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-'''function that writes a string to a text file (UTF8) and returns the number of characters written'''
+'''function that writes a string to a text file (UTF8)
+and returns the number of characters written'''
+
+
 def write_file(filename="", text=""):
     """writes string to a text file and return number of characters
 
