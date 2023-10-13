11cb87af9076a4027cbcc2a87d622e70247d1b5c
SAAJEVES <eveshofase@gmail.com>
2023-10-13 03:31:28 +0100

    base_geometry integer validator1

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,9 +4,9 @@
 
 
 def is_same_class(obj, a_class):
-    '''function that returns True if the object is 
+    '''function that returns True if the object is
     exactly an instance of the specified class ; otherwise False
-    
+
     Args:
         obj: an instance of an object
         a_class: name of a class
