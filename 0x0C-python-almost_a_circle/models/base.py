7d6fbaa7671bfb40c43c1fdd02db8095a1e0f0af
SAAJEVES <eveshofase@gmail.com>
2023-10-17 03:54:49 +0100

    base_from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,8 +2,7 @@
 
 '''Creating Base Class'''
 
-from json import dumps
-
+import json
 
 class Base:
     '''A class called Base'''
@@ -22,4 +21,25 @@ class Base:
         """Static method that converts to JSON object"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        python_list = json.loads(json_string)
+
+        if len(python_list) == 0 or python_list is None:
+            return []
+        return python_list
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+        class_name = cls.__name__
+        filename = f"{class_name}.json"
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dicts)
+        with open(filename, 'w') as file:
+            file.write(json_string)
