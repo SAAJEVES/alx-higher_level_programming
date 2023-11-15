843edd72b538e7feebfd13430350a975df0683eb
SAAJEVES <eveshofase@gmail.com>
2023-11-15 02:30:56 +0100

    first_table

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,8 +1,6 @@
 -- script that lists all the tables of a database in your MySQL server.
 --	The database name will be passed as argument of mysql command
 --	    (in the following example: mysql is the name of the database)
-SET @db = ?;
-
-USE @db;
-
+SET @my_db = ?;
+USE @my_db;
 SHOW TABLES;
