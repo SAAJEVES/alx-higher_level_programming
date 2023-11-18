8dfaa4abcc1e0bcb304e233732e8dcdd081e519a
SAAJEVES <eveshofase@gmail.com>
2023-11-18 23:10:31 +0100

    genre id3

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -31,8 +31,7 @@ CREATE TABLE `tv_shows` (
 
 USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_show_genres
+FROM tv_shows 
 JOIN tv_shows 
 ON tv_show_genres.show_id = tv_shows.id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
-
