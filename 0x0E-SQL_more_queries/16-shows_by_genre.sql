e87b9fc70c7f2cda1902b9c8990f3b7cc8e15db7
SAAJEVES <eveshofase@gmail.com>
2023-11-19 03:21:45 +0100

    not

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,6 +6,6 @@
 --	The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_genres.names
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id 
-JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id 
+LEFT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_genres.names;
