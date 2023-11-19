75da0e317294456a0ce417031606d35927cf9d55
SAAJEVES <eveshofase@gmail.com>
2023-11-19 03:00:19 +0100

    my genres1

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,8 +7,8 @@
 SELECT tv_genres.name 
 FROM tv_genres 
 JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT id 
-	FROM tv_shows 
-	WHERE title = 'Dexter')
-ORDER BY tv.genres.name ASC;
+ON tv_genres.id = tv_show_genres.genre_id 
+JOIN tv_shows 
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter' 
+ORDER BY tv.genres.name;
