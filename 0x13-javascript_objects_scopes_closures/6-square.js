8108220709241def177a4531a36422c7b446069e
SAAJEVES <eveshofase@gmail.com>
2023-12-13 05:18:27 +0100

    square_charprint

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const OldSquare = require('./5-square');
+
+class Square extends OldSquare {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
