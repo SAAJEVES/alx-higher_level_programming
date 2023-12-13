594b8ad15a3c44ad07177d9fbd3d5c3499335eaa
SAAJEVES <eveshofase@gmail.com>
2023-12-13 05:33:54 +0100

    esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const newList = [];
+  const len = list.length;
+
+  for (let i = (len - 1); i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
