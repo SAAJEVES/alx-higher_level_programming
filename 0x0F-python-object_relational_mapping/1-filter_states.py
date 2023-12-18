cf426b8c8a1c4fe942e81335e6c9ffeff4e0f7f1
SAAJEVES <eveshofase@gmail.com>
2023-12-18 20:33:55 +0100

    filter states4

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,10 +11,10 @@ def filter_N():
     """Filters through to get states that starts with N"""
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3],
-                           port=3306, charset="utf8")
+                           port=3306)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY
-                states.id ASC")
+    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%'
+                ORDER BY states.id ASC")
     query_row = cur.fetchall()
     for row in query_row:
         print(row)
