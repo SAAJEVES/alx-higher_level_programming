41626986c823a7529e0202d8d813834a34a385ff
SAAJEVES <eveshofase@gmail.com>
2023-12-19 00:59:11 +0100

    my safe filter state1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,9 +16,8 @@ if __name__ == "__main__":
                                passwd=sys.argv[2], db=sys.argv[3],
                                port=3306)
         cur = conn.cursor()
-        name = sys.argv[4]
         cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
-                    '{}'".format(sys.argv[4]))
+                    %s",(sys.argv[4],))
         query_rows = cur.fetchall()
 
         for row in query_rows:
