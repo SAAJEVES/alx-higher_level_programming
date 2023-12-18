f4c0b5154d4b8a057e078d1b4c573b98012dd32f
SAAJEVES <eveshofase@gmail.com>
2023-12-18 20:24:40 +0100

    filter states1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,7 +3,6 @@
     N (upper N) from the database hbtn_0e_0_usa
 """
 
-
 import MySQLdb
 import sys
 
@@ -11,7 +10,7 @@ import sys
 def filter_N():
     """Filters through to get states that starts with N"""
     conn = MySQLdb.connect(host="localhost", user=sys.argv[1]
-                           passwd=argv[2], db=sys.argv[3],
+                           passwd=sys.argv[2], db=sys.argv[3],
                            port=3306, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY
