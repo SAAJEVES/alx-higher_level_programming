20236ab9f4a8fe1b1ab97b98150c553b5b8355b1
SAAJEVES <eveshofase@gmail.com>
2023-12-19 01:45:07 +0100

    cities1

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,9 @@ def list_cities():
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
 
     for row in query_rows:
