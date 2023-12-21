30017c72745d03eb4eab27d68b6ba0e17754b046
SAAJEVES <eveshofase@gmail.com>
2023-12-22 00:44:39 +0100

    model_state_insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+script that adds the State object “Louisiana” to the
+database hbtn_0e_6_usa
+'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+            )
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    with Session() as session:
+        new_row = State(name=“Louisiana”)
+        session.add(new_row)
+        session.commit()
+
+        print(new_row.id)
