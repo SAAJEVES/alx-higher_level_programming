d0fe195bab0c7fddf068c5fc9841234ad56e9aba
SAAJEVES <eveshofase@gmail.com>
2023-12-21 02:20:43 +0100

    model state fetch all2

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
                 sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True
             )
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
     session = Session()
 
     if len(sys.argv) != 4:
