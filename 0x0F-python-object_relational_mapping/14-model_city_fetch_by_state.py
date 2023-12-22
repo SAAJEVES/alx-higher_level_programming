47e12c412777a501ab96c36a6a91d1770150e0d1
SAAJEVES <eveshofase@gmail.com>
2023-12-22 14:39:29 +0100

    city8

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -22,8 +22,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     with Session() as session:
-        query_res = session.query(State.name, City.id, City.name)
-                .join(City, State.id == City.state_id)
+        query_res = (session.query(State.name, City.id, City.name)
+                .filter(State.id == City.state_id))
 
         for val in query_res:
             print(f"{val[0]}: {val[1]} {val[2]}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,9 +4,8 @@ Python file similar to model_state.py named model_city.py
 that contains the class definition of a City
 '''
 
-from model_state import Base, State
+from model_state import Base
 from sqlalchemy import ForeignKey, Column, Integer, String
-from sqlalchemy.orm import relationship
 
 
 class City(Base):
