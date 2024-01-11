0e262dae27999b6110135184888be1349e85212e
SAAJEVES <eveshofase@gmail.com>
2024-01-11 08:14:51 +0100

    my github1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,8 @@
     (username and password) and uses the GitHub API to display your id
 
         - You must use Basic Authentication with a personal access token
-          as password to access to your information (only read:user permission is needed)
+          as password to access to your information
+          (only read:user permission is needed)
         - The first argument will be your username
         - The second argument will be your password
           (in your case, a personal access token as password)
